c0e5f09a6f92cc4d253c93e48fee7c37fbfad37d
alicjamarciniak <lcm@3a.pl>
2016-10-06 17:12:21 +0200

    develop/add: Added repsponsive files and sticky navbar mechanism (need fix)

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
 
+  // var num = 10; //number of pixels before modifying styles
+  // $(window).bind('scroll', function (e) {
+  //   e.preventDefault();
+  //     if ($(window).scrollTop() > num) {
+  //         $('.navbar-sticky').addClass('fixed');
+  //         $('.header').addClass('header-fixed');
+  //     } else {
+  //         $('.navbar-sticky').removeClass('fixed');
+  //         $('.header').removeClass('header-fixed');
+  //     }
+  // });
+
   $(document).on('click', function (e) {
       if (e.originalEvent.defaultPrevented) return;
       // continue
@@ -19,11 +31,38 @@ $(document).ready(function() {
     }
 
     if($next.length) {
-      $next.addClass('active-slide').animate();
+      $next.addClass('active-slide');
     } else {
       $($me).siblings($begining).addClass('active-slide');
     };
   };
+
+  // function changeSlide($direction) {
+  //   var $me = '.arrow-' + $direction;
+  //
+  //   if ($direction == 'left') {
+  //     var $next = $($me).siblings('.active-slide').fadeOut('slow', function() {
+  //     $next = $next.removeClass('active-slide').next('.single-slide');
+  //   });
+  //     var $begining = '.single-slide:first';
+  //   }
+  //   else if ($direction == 'right') {
+  //     var $next = $($me).siblings('.active-slide').fadeOut('slow', function() {
+  //     $next = $next.removeClass('active-slide').prev('.single-slide');
+  //   });
+  //     var $begining = '.single-slide:last';
+  //   }
+  //
+  //   if($next.length) {
+  //     $next.fadeIn('slow', function() {
+  //       $(this).addClass('active-slide');
+  //     });
+  //   } else {
+  //     $($me).siblings($begining).fadeIn('slow', function() {
+  //       $(this).addClass('active-slide');
+  //     });
+  //   };
+  // };
 
 
   $('.arrow-left').on('click', function() {
